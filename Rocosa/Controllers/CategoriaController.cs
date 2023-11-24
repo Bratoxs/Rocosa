724ad186b73257343cf8933ca66cd12bf6e4b5ef
Marco Bravo <marbra96@hotmail.com>
2023-11-24 17:06:47 -0500

    Creacion Proyect

---
+++ Rocosa/Controllers/CategoriaController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocosa.Datos;
+using Rocosa.Models;
+
+namespace Rocosa.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoriaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //Método para listar todos los registros dentro de la tabla
+        //Get
+        public IActionResult Index()
+        {
+            //Para obtener una lista
+            IEnumerable<Categoria> lista = _db.Categoria;
+
+            return View(lista);
+        }
+
+        //Método para crear un registro en la tabla
+        //Get
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Crear(Categoria categoria) //Recibe el modelo Categoria
+        {
+            _db.Categoria.Add(categoria);
+            _db.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Rocosa/Controllers/TipoAplicacionController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocosa.Datos;
+using Rocosa.Models;
+
+namespace Rocosa.Controllers
+{
+    public class TipoAplicacionController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public TipoAplicacionController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //Método para listar todos los registros dentro de la tabla
+        //Get
+        public IActionResult Index()
+        {
+            //Para obtener una lista
+            IEnumerable<TipoAplicacion> lista = _db.TipoAplicacion;
+
+            return View(lista);
+        }
+
+        //Método para crear un registro en la tabla
+        //Get
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Crear(TipoAplicacion tipoAplicacion) //Recibe el modelo Categoria
+        {
+            _db.TipoAplicacion.Add(tipoAplicacion);
+            _db.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
